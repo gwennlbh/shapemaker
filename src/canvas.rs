077aff52cc1ce641947bbcfc2070d0706267fc0e
Gwen Le Bihan <hey@ewen.works>
2025-02-24 02:56:28 +0100

    ⚡Make it ridiculously faster
    By parallelizing conversion of pixmap to hwv frame

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,5 +1,6 @@
 use core::panic;
-use std::{collections::HashMap, ops::Range};
+use rayon::prelude::*;
+use std::{collections::HashMap, fs::OpenOptions, io::Write, ops::Range};
 
 use itertools::Itertools as _;
 use measure_time::info_time;
@@ -625,71 +626,27 @@ impl Canvas {
         info_time!("render_to_hwc_frame");
         let (width, height) = self.resolution_to_size(resolution);
         let pixmap = self.render_to_pixmap_no_cache(width, height)?;
-        Ok(video_rs::Frame::from_shape_fn(
-            (pixmap.height() as usize, pixmap.width() as usize, 3),
-            |(y, x, c)| {
+
+        let mut data = vec![0u8; height as usize * width as usize * 3];
+        data.par_chunks_exact_mut(3)
+            .enumerate()
+            .for_each(|(index, chunk)| {
+                let x = index / (height as usize);
+                let y = index % (height as usize);
+
                 let pixel = pixmap
                     .pixel(x as u32, y as u32)
                     .expect(&format!("No pixel found at x, y = {x}, {y}"));
-                match c {
-                    0 => pixel.red(),
-                    1 => pixel.green(),
-                    2 => pixel.blue(),
-                    _ => unreachable!(),
-                }
-            },
-        ))
-        // let mut rgb_pixels = ndarray::Array3::from_shape_vec(
-        //     [pixmap.width() as usize, pixmap.height() as usize, 3],
-        //     pixmap
-        //         .pixels()
-        //         .iter()
-        //         .map(|pixel| [pixel.red(), pixel.green(), pixel.blue()])
-        //         .flatten()
-        //         .collect(),
-        // )?;
 
-        // // Go from WHC to HWC
-        // rgb_pixels = rgb_pixels.permuted_axes([1, 0, 2]);
+                chunk[0] = pixel.red();
+                chunk[1] = pixel.green();
+                chunk[2] = pixel.blue();
+            });
 
-        // println!("rgb_pixels: {:?}", rgb_pixels.shape());
-
-        // // Flatten before giving to YUV conversion
-        // let rgb_pixels_flat = rgb_pixels.into_flat();
-
-        // println!("rgb_pixels_flat: {:?}", rgb_pixels_flat.len());
-
-        // let yuv_pixels = convert_rgb_to_yuv420p(
-        //     &rgb_pixels_flat
-        //         .as_slice()
-        //         .expect("Failed to convert HWC RGB data to a slice for YUV420p conversion"),
-        //     pixmap.height(),
-        //     pixmap.width(),
-        //     3,
-        // );
-
-        // println!("yuv_pixels: {:?}", yuv_pixels.len());
-
-        // // info_time!("render_to_hwc_frame -> convert_rgb_to_yuv420p");
-        // // let yuv_pixels = convert_rgb_to_yuv420p(
-        // //     pixmap.data(),
-        // //     pixmap.width(),
-        // //     pixmap.height(),
-        // //     tiny_skia::BYTES_PER_PIXEL,
-        // // );
-
-        // // println!("yuv_pixels: {:?}", yuv_pixels.len());
-
-        // info_time!("render_to_hwc_frame -> create_frame");
-
-        // let frame_data = ndarray::Array3::from_shape_vec(
-        //     [pixmap.height() as usize, pixmap.width() as usize, 3],
-        //     yuv_pixels,
-        // )?;
-
-        // println!("frame_data: {:?}", frame_data.shape());
-
-        // Ok(video_rs::Frame::from(frame_data))
+        Ok(video_rs::Frame::from_shape_vec(
+            [height as usize, width as usize, 3],
+            data,
+        )?)
     }
 
     fn usvg_tree_to_pixmap(
