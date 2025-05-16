43319e8c8c2f1c35068e9a726a05cb4a5ad6f021
gwennlbh <gwennlbh@users.noreply.github.com>
2025-05-16 18:15:35 +0000

    deploy: 57f5e1acb903169f0ddf909f411c1d32eae4f271

--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["shapemaker",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"shapemaker/vst/remote_probe/struct.RemoteProbe.html\" title=\"struct shapemaker::vst::remote_probe::RemoteProbe\">RemoteProbe</a>"]]]]);
+    var implementors = Object.fromEntries([["shapemaker",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"shapemaker/vst/remote_probe/struct.RemoteProbe.html\" title=\"struct shapemaker::vst::remote_probe::RemoteProbe\">RemoteProbe</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
