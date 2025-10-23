06bf978074a0452b09baaf7f2783397b9ccf972e
gwennlbh <gwennlbh@users.noreply.github.com>
2025-10-23 23:04:40 +0000

    🎨 Format code

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,8 @@ pub mod vst;
 pub use geometry::{Angle, Axis, Containable, Point, Region};
 pub use graphics::{
     Canvas, Color, Color::*, ColorMapping, ColoredObject, Fill, Filter,
-    FilterType, Layer, LineSegment, Object, Object::*, ObjectSizes, Transformation,
+    FilterType, Layer, LineSegment, Object, Object::*, ObjectSizes,
+    Transformation,
 };
 pub use rendering::{
     fonts, CSSRenderable, SVGAttributesRenderable, SVGRenderable,
